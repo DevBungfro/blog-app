e850806f372ae342253e37b19a20438c39a2bebf
DevBungfro <85974928+DevBungfro@users.noreply.github.com>
2022-03-17 18:01:28 +0200

    express

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+require("dotenv").config()
 const express = require('express');
 const app = express();
+const path = require("path");
+const dataDir = path.resolve(`${process.cwd()}${path.sep}`);
+
+app.set("view engine", "ejs");
+app.use("/", express.static(path.resolve(`${dataDir}${path.sep}assets`)));
 
 app.get('/', (req, res) => {
-    res.render('')
+    res.render('home.ejs')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('listening on port ' + process.env.PORT)
 })
